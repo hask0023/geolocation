5750680e298c5c5c0024632ce4047c49c052f0e5
shaskins <hask0023@algonquinlive.com>
2015-01-27 12:30:28 -0500

    mark 2

--- main.js
+++ main.js
@@ -18,12 +18,15 @@ function reportPosition( position ){
     var lat = position.coords.latitude;
     var long = position.coords.longitude;
     var canvas = document.createElement("canvas");
+    var outputDiv = document.createElement("div");
+    outputDiv.setAttribute("id", "output");
     canvas.setAttribute("width", 400);
     canvas.setAttribute("height", 400);
     var context = canvas.getContext('2d');
     var staticImage = "https://maps.googleapis.com/maps/api/staticmap?zoom=14&size=400x400&markers=color:blue|" + lat + "," + long + "&key=AIzaSyA8BTwbN0s9UfefqLk9vs32OSNJQz22Iag";
     
     document.body.appendChild(canvas);
+    document.body.appendChild(outputDiv);
     
     var img = document.createElement("img");
    img.onload = function() {
@@ -32,13 +35,11 @@ function reportPosition( position ){
   img.src = staticImage;
     
     
-//  output.innerHTML += "Latitude: " + position.coords.latitude + "&deg;<br/>"
-//  + "Longitude: " + position.coords.longitude + "&deg;<br/>"
-//  + "Accuracy: " + position.coords.accuracy + "m<br/>"
-//  + "Altitude: " + position.coords.altitude + " m<br/>"
-//  + "Heading: " + position.coords.heading + " &deg;<br/>"
-//  + "Speed: " + position.coords.speed + " m/s<br/>"
-//  + "Timestamp: " + position.timestamp;
+ var output = document.querySelector("#output");
+  output.innerHTML += "Latitude: " + position.coords.latitude + "&deg;<br/>"
+  + "Longitude: " + position.coords.longitude + "&deg;<br/>"
+  + "Accuracy: " + position.coords.accuracy + "m<br/>"
+  + "Altitude: " + position.coords.altitude + " m<br/>"    
 }
 
 function gpsError( error ){   
